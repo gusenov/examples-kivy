04075aaa6124ab04f7fbaa3a0880829bfd45bfbe
Аббас Гусенов <gusenov@live.ru>
2025-07-31 14:36:55 +0500

    Установка Kivy.

---
+++ 3-install-kivy.sh
@@ -0,0 +1,12 @@
+# The simplest is to install the current stable version of kivy and optionally kivy_examples from the kivy-team provided PyPi wheels. Simply do:
+
+python -m pip install "kivy[base]" kivy_examples
+
+# https://pypi.org/project/Kivy/
+# https://pypi.org/project/Kivy-examples/
+
+# This also installs the minimum dependencies of Kivy. 
+# To additionally install Kivy with audio/video support, install either kivy[base,media] or kivy[full]. 
+# See Kivy’s dependencies for the list of selectors.
+
+# https://kivy.org/doc/stable/gettingstarted/installation.html
