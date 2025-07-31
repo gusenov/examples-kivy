fd31ca67ac90807ce27ffb3bfb70f83c6425aed6
Аббас Гусенов <gusenov@live.ru>
2025-07-31 14:36:29 +0500

    Создание виртуальной среды.

---
+++ 1-create-venv.sh
@@ -0,0 +1,8 @@
+# Create a new virtual environment for your Kivy project. 
+# A virtual environment will prevent possible installation conflicts with other Python versions and packages. 
+# It’s optional but strongly recommended:
+
+# Create the virtual environment named kivy_venv in your current directory:
+python -m venv kivy_venv
+
+# https://kivy.org/doc/stable/gettingstarted/installation.html
