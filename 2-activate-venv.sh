c7b09735ef8a4e7afac76f207ce51e738edc6b99
Аббас Гусенов <gusenov@live.ru>
2025-07-31 14:36:41 +0500

    Активация виртуальной среды.

---
+++ 2-activate-venv.sh
@@ -0,0 +1,11 @@
+# Activate the virtual environment. 
+# You will have to do this step from the current directory every time you start a new terminal. 
+# This sets up the environment so the new kivy_venv Python is used.
+
+# If you are in linux or macOS, instead do:
+source kivy_venv/bin/activate
+
+# Your terminal should now preface the path with something like (kivy_venv), 
+# indicating that the kivy_venv environment is active.
+
+# https://kivy.org/doc/stable/gettingstarted/installation.html
